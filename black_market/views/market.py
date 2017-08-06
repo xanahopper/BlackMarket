2d09339608a1b69bad0a2a7368d53426c8e0ad3d
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-06 16:38:06 +0800

    add page view of index page

--- black_market/views/market.py
+++ black_market/views/market.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
 
+from black_market.libs.cache.redis import mc
+
 bp = Blueprint('market', __name__)
+
+index_page_view_count_cache_key = 'black:market:index:view:count'
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    mc.incr(index_page_view_count_cache_key)
+    view_count = int(mc.get(index_page_view_count_cache_key))
+    return render_template('index.html', view_count=view_count)
