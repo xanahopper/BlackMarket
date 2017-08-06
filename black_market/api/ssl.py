f4b1183b12236a21b4f42965a6039db13bf452b0
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-06 23:52:18 +0800

    update ssl_fileauth_text

--- black_market/api/ssl.py
+++ black_market/api/ssl.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 
-from black_market.libs.cache.redis import mc
+from black_market.config import SSL_FILEAUTH_TEXT
+
 
 bp = Blueprint('ssl', __name__)
 
 
 @bp.route('/.well-known/pki-validation/fileauth.txt', methods=['GET'])
 def ssl():
-    return '201704061708400y3tan88ebt3rzhgcrzdy2q9o63q3iz95200g9cldqjwpth9iv'
+    return SSL_FILEAUTH_TEXT
