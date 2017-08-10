f606a5a060e1d945e38a356db767e506f137d988
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-10 22:29:35 +0800

    fix

--- black_market/api/error.py
+++ black_market/api/error.py
@@ -11,9 +11,11 @@ bp = Blueprint('errors', __name__)
 @bp.app_errorhandler(404)
 @bp.app_errorhandler(405)
 def handle_4xx_error(error):
-    return jsonify(errmsg=error.description, code=0), error.code
+    # return jsonify(errmsg=error.description, code=0), error.code
+    raise error
 
 
 @bp.app_errorhandler(BlackMarketError)
 def handle_biz_error(error):
-    return jsonify(errmsg=error.message, code=error.code), error.http_status_code
+    raise error
+    # return jsonify(errmsg=error.message, code=error.code), error.http_status_code
