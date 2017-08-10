32f56eca3f9dfcf56e9d5a7547b30af0927ceaf6
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-10 19:02:38 +0800

    fix error handler

--- black_market/api/error.py
+++ black_market/api/error.py
@@ -1,5 +1,7 @@
 from flask import jsonify, Blueprint
 
+from black_market.model.exceptions import BlackMarketError
+
 bp = Blueprint('errors', __name__)
 
 
@@ -9,4 +11,9 @@ bp = Blueprint('errors', __name__)
 @bp.app_errorhandler(404)
 @bp.app_errorhandler(405)
 def handle_4xx_error(error):
-    return jsonify(errmsg=error.message), error.http_status_code
+    return jsonify(errmsg=error.description, code=0), error.code
+
+
+@bp.app_errorhandler(BlackMarketError)
+def handle_biz_error(error):
+    return jsonify(errmsg=error.message, code=error.code), error.http_status_code
