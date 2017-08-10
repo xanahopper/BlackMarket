4799dee146f827dbd690d967faa08671b2c1f37e
Xana Hopper <xanahopper@163.com>
2017-08-10 17:51:40 +0800

    Add wechat app

---
+++ weapp/app.js
@@ -0,0 +1,123 @@
+//app.js
+import Promise from './utils/bluebird';
+import {
+  SessionExpiredError,
+  LoginError,
+  SessionError, 
+  UserInfoError, 
+  BindInfoError, 
+  ServerError, 
+  NetworkError, 
+  EmptyLocalBindError,
+  ResponseError,
+  ErrorTypes
+} from './utils/exception';
+import wxw from './utils/wrapper';
+
+App({
+  globalData: {
+    userInfo: null,
+    bindInfo: null,
+    session: null,
+  },
+
+  constants: {
+    requestUrl: "https://pkublackmarket.cn/api/v1/wechat/"
+  },
+
+  urls: {
+    code2sessionUrl: "https://pkublackmarket.cn/api/v1/wechat/jscode2session",
+    checkSessionUrl: "https://pkublackmarket.cn/api/v1/wechat/check_session",
+  },
+
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    // this.checkLogin()
+    console.log('App onLaunch')
+  },
+
+  checkLogin: function () {
+    var that = this
+    console.log('Start check login')
+    return wxw.checkSession()
+      .then(() => {
+        console.log('check wechat session successfully.')
+        console.log('start get 3rd-session')
+        return wxw.getSession()
+          .then(session => {
+            console.log('get 3rd-session ok. Start check 3rd-session')
+            return wxw.checkServerSession(session)
+          })
+          .catch(err => {
+            console.log('no local session')
+            return Promise.reject(new SessionError())
+          })
+      })
+      .catch(err => {
+        if (err.type) {
+          switch (err.type) {
+            case ErrorTypes.Response:       // checkServerSession时服务器返回但是出错
+            case ErrorTypes.SessionExpired: // checkSession超时
+            case ErrorTypes.Session:        // getSession错误
+              console.log('start login', err, err.stack)
+              return wxw.wxLogin()
+                .then(code => wxw.loginCode2Session(code))
+                .catch(err => Promise.reject(err))  // LoginError || NetworkError || ServerError
+            default:
+              return Promise.reject(err)  // 
+          }
+        } else {
+          return Promise.reject(err)
+        }
+      })
+      .then(session => {
+        that.globalData.session = session
+        return wxw.getUserInfo(session)
+      })
+      .then(res => {
+        that.globalData.userInfo = res.data.userInfo
+        return wxw.uploadUserInfo(res.session, res.data)
+      })
+      .then(session => wxw.getLocalStudentInfo(session))
+      .catch(err => {
+        if (err.type && err.type == ErrorTypes.EmptyLocalBind) {
+          return wxw.fetchServerStudentInfo(err.session)
+        } else {
+          return Promise.reject(err)
+        }
+      })
+      .then(res => {
+        console.log(res)
+        that.globalData.bindInfo = res
+        return Promise.resolve(that.globalData)
+      })
+      .catch(err => {
+        console.error(err)
+        if (err.type) {
+          switch (err.type) {
+            case ErrorTypes.Login:
+            case ErrorTypes.UserInfo:
+              wx.redirectTo({
+                url: '/pages/no_auth/no_auth'
+              })
+              break
+            case ErrorTypes.Server:
+            case ErrorTypes.Response:
+            default:
+              wx.redirectTo({
+                url: '/pages/error/error'
+              })
+              break
+            case ErrorTypes.BindInfo:
+              wx.redirectTo({
+                url: '/pages/auth/auth'
+              })
+          }
+        } else {
+          wx.redirectTo({
+            url: '/pages/error/error'
+          })
+        }
+      })
+  }
+})
---
+++ weapp/pages/index/index.js
@@ -0,0 +1,121 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    userInfo: {},
+    filterShowed: false,
+    sortShowed: false,
+    sortState: 3,
+    inputShowed: false,
+    inputVal: "",
+    hasMore: true,
+    posts: [{
+      postId: 1,
+      supply: '奇葩学',
+      time: '2017-08-06',
+      state: 0
+    }, {
+      postId: 1,
+      supply: '奇葩学',
+      time: '2017-08-06',
+      state: 0
+    }, {
+      postId: 1,
+      supply: '奇葩学',
+      time: '2017-08-06',
+      state: 0
+    }, {
+      postId: 1,
+      supply: '奇葩学',
+      time: '2017-08-06',
+      state: 0
+    }, {
+      postId: 1,
+      supply: '奇葩学',
+      time: '2017-08-06',
+      state: 0
+    }]
+  },
+  onLoad: function () {
+    // console.log('onLoad')
+    // var that = this
+    // wx.showLoading({
+    //   title: '加载中',
+    //   mask: true
+    // })
+    // app.checkLogin()
+    // .then(data => {
+    //   if (data) that.setData(data)
+    //   wx.hideLoading()
+    // })
+    if (app.globalData.session && app.globalData.userInfo && app.globalData.bindInfo) {
+      this.setData(app.globalData)
+    } else {
+      wx.redirectTo({
+        url: '../splash/splash'
+      })
+    }
+  },
+  showInput: function () {
+      this.setData({
+          inputShowed: true
+      });
+  },
+  hideInput: function () {
+      this.setData({
+          inputVal: "",
+          inputShowed: false
+      });
+  },
+  clearInput: function () {
+      this.setData({
+          inputVal: ""
+      });
+  },
+  inputTyping: function (e) {
+      this.setData({
+          inputVal: e.detail.value
+      });
+  },
+  toggleSort(e) {
+    let that = this
+    this.setData({
+      sortShowed: !that.data.sortShowed
+    })
+  },
+  hideSort() {
+    this.setData({
+      sortShowed: false
+    })
+  },
+  cancelMask() {
+    this.hideSort()
+  },
+  setSortState(e) {
+    if (e.currentTarget.dataset.state) {
+      this.setData({
+        sortState: Number.parseInt(e.currentTarget.dataset.state)
+      })
+    }
+    this.cancelMask()
+  },
+  toggleTimeSort(e) {
+    var state = 0
+    switch(this.data.sortState) {
+      case 0:
+      case 1:
+      case 2:
+      case 4:
+        state = 3
+        break
+      case 3:
+        state = 4
+        break
+    }
+    this.setData({
+      sortState: state
+    })
+    this.cancelMask()
+  }
+})
