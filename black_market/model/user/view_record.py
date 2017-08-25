c0aef57d3caef1b5c0ef76ce38e1c31bb77cf4ab
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-25 19:41:56 +0800

    fix delete view_record

--- black_market/model/user/view_record.py
+++ black_market/model/user/view_record.py
@@ -54,7 +54,8 @@ class ViewRecord(db.Model):
 
     @classmethod
     def delete_records_by_post(cls, post_id, post_type):
-        cls.query.filter_by(post_id=post_id, post_type=post_type.value).delete()
+        cls.query.filter_by(post_id=post_id, post_type=post_type.value).delete(
+            synchronize_session='fetch')
 
     def clear_cache(self):
         mc.delete(self._records_by_student_and_post_and_type_cache_key % (
