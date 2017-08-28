45acfab2a221122601ab02d435d69e3f1a39bbdc
Wang Zhihao <wangzhihao0629@gmail.com>
2017-08-28 10:18:44 +0800

    register task

--- task/config.py
+++ task/config.py
@@ -8,4 +8,4 @@ CELERY_TIMEZONE = get_localzone().zone
 BROKER_URL = CELERY_BROKER_URL
 CELERY_RESULT_BACKEND = CELERY_RESULT_BACKEND
 
-CELERY_INCLUDE = ('task.tasks',)
+CELERY_INCLUDE = ('black_market.model.user.student',)
